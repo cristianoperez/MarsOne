dbd996926e8e75ce62a2fd4bc504342a3a701331
Cristiano <Cristiano@192.168.0.192>
2015-09-07 20:31:43 -0300

    Adicionado tratamento de exceção

--- src/main/java/com/cristiano/vo/handler/AbstractListHandler.java
+++ src/main/java/com/cristiano/vo/handler/AbstractListHandler.java
@@ -2,13 +2,21 @@ package com.cristiano.vo.handler;
 
 import java.util.List;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 public abstract class AbstractListHandler<T> {
 
 	private List<T> value;
 	private String originalValue;
 
 	public AbstractListHandler(String str) {
-		parse(str);
+		try{
+			parse(str);
+		} catch(Exception e){
+			throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity("Parametro invalido, verifique se os parametros e o formato esta correto. " +e.getMessage()).build());
+		}
 	}
 
 	public abstract void parse(String str);
